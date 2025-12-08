e83692e89364476dbc989c984fdea4b285e5fbcf
QuickPrimeTech <quickprimetech@gmail.com>
2025-12-08 13:05:28 +0300

    finished making the menu highlights to use the popular section and adjust to all themes

--- src/app/menu/page.tsx
+++ src/app/menu/page.tsx
@@ -2,45 +2,56 @@ import api from "@/lib/api-client";
 import MenuContent from "@/sections/menu/menu-content";
 import { ApiResponse } from "@/types/api";
 import { MenuItem } from "@/types/menu";
+import { truncate } from "@/utils/text-formatters";
 import { Metadata } from "next";
 
-export const metadata: Metadata = {
-  title: "Our Menu | Delicious Meals, Drinks & Specials",
-  description:
-    "Explore our full restaurant menu. Discover delicious meals, drinks, desserts, and chef specials made fresh every day.",
-  openGraph: {
-    title: "Ziwa Restaurant Menu",
-    description:
-      "Browse our full selection of meals, drinks, and chef-crafted specials.",
-    url: "https://ziwa-nu.vercel.app/menu",
-    type: "website",
-    images: [
-      {
-        url: "https://res.cloudinary.com/quick-prime-tech/image/upload/v1764935940/ziwa-restaurant/eldoret/offers/axjmqjxgo4aebbylsbn1.jpg", // optional — replace with your real menu OG image
-        width: 1200,
-        height: 630,
-        alt: "Restaurant Menu Preview",
-      },
-    ],
-  },
-  twitter: {
-    card: "summary_large_image",
-    title: "Restaurant Menu",
-    description: "Explore our delicious meals, drinks, and daily specials.",
-    images: ["https://yourdomain.com/og/menu-banner.jpg"],
-  },
-  alternates: {
-    canonical: "https://yourdomain.com/menu",
-  },
-};
-
-export default async function MenuPage({
+export async function generateMetadata({
   searchParams,
 }: {
   searchParams: Promise<{ "selected-item"?: string }>;
-}) {
-  const selectedItem = (await searchParams)["selected-item"];
+}): Promise<Metadata> {
+  const slug = (await searchParams)["selected-item"];
 
+  try {
+    // Fetch the menu item by slug
+    const { data: menuItem } = await api.get<ApiResponse<MenuItem>>(
+      `/menu-items/slug/${slug}`
+    );
+
+    return {
+      title: `${menuItem.name}`,
+      description: truncate(menuItem.description, 160),
+      openGraph: {
+        title: `${menuItem.name} | Special Offer`,
+        description: truncate(menuItem.description, 160),
+        url: `https://ziwa-nu.vercel.app/menu?selected-item=${menuItem.slug}`,
+        type: "website",
+        images: [
+          {
+            url: menuItem.image_url || "",
+            width: 1200,
+            height: 630,
+            alt: menuItem.name,
+          },
+        ],
+      },
+      twitter: {
+        card: "summary_large_image",
+        title: `${menuItem.name}`,
+        description: truncate(menuItem.description, 160),
+        images: [menuItem.image_url || ""],
+      },
+    };
+  } catch {
+    return {
+      title: "Menu Item Not Found",
+      description:
+        "This menu item or dish does not exist or is no longer available.",
+    };
+  }
+}
+
+export default async function MenuPage() {
   const { data: menuItems } = await api.get<ApiResponse<MenuItem[]>>(
     "/menu-items"
   );
